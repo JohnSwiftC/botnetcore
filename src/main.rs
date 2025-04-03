ebcd3f80b275e0cf9911d12196e7f5ea13f1b3d2
JohnSwiftC <johnswift@live.com>
2025-04-03 17:58:16 -0500

    peer connections?

--- src/main.rs
+++ src/main.rs
@@ -8,49 +8,53 @@ use rsa::pkcs1v15::{SigningKey, Signature};
 use rsa::signature::{RandomizedSigner, SignatureEncoding, Verifier};
 use rsa::sha2::{Digest, Sha256};
 use rsa::pkcs1::DecodeRsaPrivateKey;
-
+use std::collections::HashSet;
+use std::net::SocketAddr;
 use rand;
 
-const CONTROL: &str = "127.0.0.1:8080";
+const CONTROL: &str = "0.0.0.0:8080";
 const PRIVATE_KEY_PEM: &str = "-----BEGIN RSA PRIVATE KEY-----
-MIIEpgIBAAKCAQEA3Q4YyAa124SngD/gET8G+2y5tECs0DID/M2TftoLPcnKlfpi
-ZOSPS2bz9bWA33g0NgE3wGBFtYWevQVMwdU18HR6V5BF1nkmF9JOOHGzcTPXTFHa
-UKPVh13+njkQhuEk8y0wNDSz58mEdHAKL/ckyMFl2o1Z5qrAoGO0+v6stUcD/nrr
-bdvosfkJmUo3aVQTy2avtX94h8tp1uLp6aFV/5IvegW3FLAYMbDHkCV8VPzwRVl5
-mSLQIrQKfht+L7Kb7aWTKxcDS+o2oHRIpZWSlE3WvG4xHNp0lhiSJs3i+m0loTqu
-efnewanT2eKi+DC7sokMR16nvIyJGz0TGISzNQIDAQABAoIBAQC5Bzum8jb2b6rk
-ppKe3OqzOOaYAiqkebjsugZjf485QNbZgqMpYEaxDpPVbv9YDf1l1s8br3mp3xZn
-kTAWbqifhF+DO7e5Nd60wcvHYySX9JSZxUm/heMNU5f0SD+VJo8PHRvkFtkfgoXk
-EIP2ch/QClF/BiHh+bsShWLPVKyrbh+kRrioi5v8wBGyaKo9NXAjygdF7ATnZctb
-w9Y6r9M/APZftaE0TKW2/WvnpWbnHE8x4mktN6e5IsjEKClcDTFUA6d7YsFhHW3M
-cCYcwRWRBGp/eK+82NSZld2unO0Z+Gs5CYFmNUQIpxm16ta5oqPcdw2MZ9KqOah7
-NwPBXiZhAoGBAO2FUoSwXGgj++WitCB+un3nA6NxmQ4rBPNVN5N0jqaSvdwqyudG
-iWSnj1GzmWK3ONo3AzEfVxV6C3G1aTmQd6jEzYhC14/c/T1PyCwOh7NMe5lNEFvT
-nLQ87b9PsSM/wWruJsoGK3qbMJtBydtXZobOs57lGafopjmLwINRYf5/AoGBAO5A
-1AphCfQHAdCCinT4kH0FmqlUf5WKwh6KQogUzCIohpIaMlUnWjaHKqsdF4MtE52f
-5S8Nj3LmIttuKCS4EHBCsXXMnDQ1Zj16fwALPd9+p+L+YO+Bk2OZXtVbrG475U/E
-kLnuHnZhCT67GE9kMLNOtSlFMtJrfkkKp3ac5txLAoGBAMyZLJS56k+Wt5R+i9T1
-9SUgywSOhP4sVm5ZOK9fFsYr3zn47qE8JHu8znrdT0I2e9WUz74Ptg78+0CuuP5q
-EHkjVbyLf24gOVAIGnDA72mmd019kZrX9MNZPJ6wQfEQKUc379fUar2JUtxaVkwH
-xHxZL2avCGOXxyAI3wL/Jn93AoGBAMBt09TeM2/JCQojpTh8WhbgI96HxNQ/UjHg
-cYP/aDBekvIF/okQU6ie2zOBN7BXA/TXY//w799ZSOX5vqqJj51cAICoVbFFP+J/
-wXuDmOGeJnMMCbbn7OcQxKuGc2pFLhyTImFJlYj/Lrxf2G6m+T6ZX3CJ11ZkQ4cy
-97axWzSpAoGBAIkm5UaCxstydRrxJr5VfLEAR7ey2BhsoFGgDC3UkCUZ+omODJd3
-Iu3kriS1o/1RbuWsOkrq80TfuDvSdB74tN9wCcqVRHNYV2IjW9XAe2+P0LiJCHz7
-cq+mnghyfeaFOHgcwZSCMDW5oNDC8QleSzQsWwNCDENlfEfYC+Cju7fd
+MIIEowIBAAKCAQEA1KBXWBk/GxsfTWhKTSUp7SB/M+cE9vQHVYaeRgtzN4t430Jb
+mUbU3bMVusTc5FzE659d7JFpFmEV1jQuuscwarBtG4LTsgRzm/WntGlXLXqCLlr/
+lka01f5Lc+myAWj+fVGmFJArC20cdo2KEJXVnQnm9VYzUI0yz9Y0lyvBJYCoYqoX
+mX+ZJE6D/VrgZY5XSmeiDUeV5W8+wFUj7VE89B9agzKXjz3K1xUsrz7Q5Ri9XQ+F
+7ouLghP0P975jWSP2sHbRdtXSei7zb4baGOiOe/7mH1/xUSGmxyHuQlc0haOGUXC
+7jxT2jefOObS8Jry+lGlMMYpWSa5YazWBF+wnwIDAQABAoIBAHz0+j/hHnAcoWnJ
+d5kc1SXxajAjfYxwX3sI0S1S/1ROWCvFALX0pZbg701C8poWVvguQXGNB1hKmB25
+rxi/hRm+M3DukuDUsuieFv/0xHIKxETf38L+LLpyZv9pAN7JSH53pIYDL/jYF9ko
+V1Jl7BMNRbk6/o9BW/I+7ctFwI37mi4iXwgOtYbhqd97MiNMrw4RuvZskhnUhTnz
+5L6piNdPASxb2eJSb5DzmIG7xr8AwVoFB86ErPWyCsE4LT258fNQjU+RcCBeMQP+
+rxvnDBN3dcsYDWcNRBBjE+OyEgBnxZNuxHIPKVhiqfyqCti485a8gDMIF1JCTNEf
+VTL5Q7ECgYEA/ti4kRVmGzFXhDN0/KTwOItmF7XvlnyZRJ6kO4K8nZIVk65QP9aS
+KPdDjrX2KRoiCRAzz87O+8oIrbnjoIvPuUMc+oqy3RVQDDqNlypKNC8A8ID87j46
+FaO6RbccV1upKGsht7NqkIz+sQMtdF4E7hrSA9gxcdUk0j9xr+kfyDcCgYEA1Zaz
+mmZ4VKs0hD6OCr0wabdxhKaJTnhWr0Cw1IMhv/rO8WiHSzDqNKbys50Jxh3nwN01
+xrQ41SzT3yocWjkiQA+6TYf3aAchZohPjmbNlXRf2bWW/chY8FISP7qsQCuXzAhI
+KcXVFWYYMVcH5A+iA62/OGIrlt7/WZn2/ab31tkCgYEAmw287fj5EN5qgvLT4Mtx
+pNWbnh2B0iupDQkAn3yhybUSD7Jk/RJOB0K6BeBMDyHpR15mR69qW+PGd/y7s/ic
+v/lHbmBpBv5qn6YYc7q1Px7CEZJJUaLmsDfXo4cPZU9NklvsPdqZoa1HYOewBjcW
+BNCoqtIk8z9dkdYsE31hau0CgYAtcpCq5OzrzzYwgYMF4/W+OOttt2X2DYMcbV5z
+CagPypVoJr7LEBB1vaRc9ahTwJ9EmY1Mx6JkMdKvZK19wTR5fx88ShcpyoKf55dO
+DK/oAkg28f276HqMQFpjtxvZ16zIHGcGhHLvx7aUIRmrvGvT7o+GttThmHerICi/
+Fl0cGQKBgAfnceKHOuz/tQ8in1Wb17O0Mt+m0z9/lUc69JV4mSur/2MSjjKsB1Mu
+N+sXt8QolJCKjIn0/pKnDNcY1ZPwpfwFHQ77Mg6OlItW4i7CBpQoP5CVPOaObwlg
+hNRAhMwvEJWBcTVrjAhWACOT3KtCAZojlluwnaNSJqnfJBjn/4Zl
 -----END RSA PRIVATE KEY-----";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind(CONTROL).await?;
+    let mut unique_connections: HashSet<SocketAddr> = HashSet::new();
 
     let args: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(std::env::args().collect()));
 
     let private_key = RsaPrivateKey::from_pkcs1_pem(PRIVATE_KEY_PEM).expect("Could not get private key");
     let signing_key: Arc<Mutex<SigningKey<Sha256>>> = Arc::new(Mutex::new(SigningKey::<Sha256>::new(private_key)));
 
+    let mut conns = 0;
     loop {
-        let (socket, _) = listener.accept().await?;
+        let (socket, peer_addr) = listener.accept().await?;
+
         let a = Arc::clone(&args);
         let s = Arc::clone(&signing_key);
         tokio::spawn(async move {
